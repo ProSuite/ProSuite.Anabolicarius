9f5accaee87341b1639dc814267d32d7cc12144d
paluchs <patrick.lu@bluewin.ch>
2025-01-08 09:28:41 +0100

    Basic working implementation of cli

--- prosuite_anabolicarius/__init__.py
+++ prosuite_anabolicarius/__init__.py
@@ -1,10 +1,1 @@
-import click
 
-
-@click.group()
-def anabolicarius():
-    """Main CLI application."""
-    pass
-
-if __name__ == "__main__":
-    anabolicarius()
--- prosuite_anabolicarius/cli.py
+++ prosuite_anabolicarius/cli.py
@@ -1,10 +1,27 @@
 from typing import Literal
 
 import click
-from prosuite_anabolicarius import anabolicarius
+
+
+@click.group()
+def anabolicarius():
+    """
+    Entry point for the cli.
+    :return:
+    """
+    pass
 
 
 @anabolicarius.command()
 @click.option('--db', default='oracle', help = 'For which db type you want to initialize you example db (postgres or oracle).')
-def init(db_type: Literal['postgres', 'oracle']):
+def init(db: Literal['postgres', 'oracle']):
+    """
+    Initialize a new db container.
+    :param db:
+    :return:
+    """
     click.echo('Initializing database...')
+
+
+if __name__ == "__main__":
+    anabolicarius()
