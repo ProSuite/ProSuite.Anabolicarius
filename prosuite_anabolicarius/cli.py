56c654158adc3b98941ccac34dd362fef02b1275
paluchs <patrick.lu@bluewin.ch>
2025-01-08 09:53:47 +0100

    Defined basic API of cli

--- prosuite_anabolicarius/cli.py
+++ prosuite_anabolicarius/cli.py
@@ -22,6 +22,44 @@ def init(db: Literal['postgres', 'oracle']):
     """
     click.echo('Initializing database...')
 
+@anabolicarius.command()
+def run():
+    """
+    Run a database container.
+    :return:
+    """
+    click.echo('Running db container...')
+
+@anabolicarius.command()
+def stop():
+    """
+    Stop a database container.
+    :return:
+    """
+    click.echo('Stopping db container...')
+
+@anabolicarius.command()
+def build():
+    """
+    Build a db container to test the ProSuite.
+    :return:
+    """
+    click.echo('Building db container...')
+
+@anabolicarius.command()
+def push():
+    """
+    Push a local db container to a container registry.
+    :return:
+    """
+
+@anabolicarius.command()
+def registry_init():
+    """
+    Initialize a db container registry.
+    :return:
+    """
+    click.echo('Initializing db container registry...')
 
 if __name__ == "__main__":
     anabolicarius()
